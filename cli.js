983e4e47a3b56556d354a8a2772963c93941fba3
Andrey Sitnik <andrey@sitnik.ru>
2018-10-25 19:27:02 -0400

    Do not use postinstall script

--- cli.js
+++ cli.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
 
-require('./warn')
+var chalk = require('chalk')
+
+var r = chalk.red
+var y = chalk.yellow
+var b = chalk.yellow.bold
+
+process.stderr.write(
+  '\n\n' +
+  r('    ╭─────────────────────────────────╮\n') +
+  r('    │                                 │\n') +
+  r('    │  You made typo in') + y(' browser') + b('s') + y('list') + r('  │\n') +
+  r('    │                                 │\n') +
+  r('    ╰─────────────────────────────────╯\n') +
+  '\n\n'
+)
 process.exit(1)
